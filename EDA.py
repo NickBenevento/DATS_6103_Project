e9bf463fb11345d1ee5c233e77c418ee7f54d539
angeolson <78054238+angeolson@users.noreply.github.com>
2022-03-31 20:49:17 -0400

    Update EDA.py

--- EDA.py
+++ EDA.py
@@ -53,14 +53,46 @@ p
 
 # create crosstab
 ct=pd.crosstab(columns=df['Race'],index=df['HeartDisease'])
+print(ct)
+
+# define vars to make proportions
+dfwhite = df[ df['Race'] == "White" ]
+white = dfwhite.count()[0]
+
+dfaian = df[ df['Race'] == "American Indian/Alaskan Native" ]
+aian = dfaian.count()[0]
+
+dfasian = df[ df['Race'] == "Asian" ]
+asian = dfasian.count()[0]
+
+dfblack = df[ df['Race'] == "Black" ]
+black = dfblack.count()[0]
+
+dfhis = df[ df['Race'] == "Hispanic" ]
+his = dfhis.count()[0]
+
+dfoth = df[ df['Race'] == "Other" ]
+oth = dfoth.count()[0]
 
 # now stack and reset
+
 stacked = ct.stack().reset_index().rename(columns={0:'value'})
 
+# create proportion column
+stacked['valueProp'] = 0
+stacked.loc[ stacked['Race'] == "White", 'valueProp'] = stacked['value']/white
+stacked.loc[ stacked['Race'] == "American Indian/Alaskan Native", 'valueProp'] = stacked['value']/aian
+stacked.loc[ stacked['Race'] == "Asian", 'valueProp'] = stacked['value']/asian
+stacked.loc[ stacked['Race'] == "Black", 'valueProp'] = stacked['value']/black
+stacked.loc[ stacked['Race'] == "Hispanic", 'valueProp'] = stacked['value']/his
+stacked.loc[ stacked['Race'] == "Other", 'valueProp'] = stacked['value']/oth
+
+
 # plot grouped bar chart
-p = sns.barplot(x=stacked.Race, y=stacked.value, hue=stacked.HeartDisease)
+p = sns.barplot(x=stacked.Race, y=stacked.valueProp, hue=stacked.HeartDisease)
 sns.move_legend(p, bbox_to_anchor=(1, 1.02), loc='upper left')
-p
+p.set(ylabel = "Share of Total Pop.")
+plt.show()
 
 
 # %%

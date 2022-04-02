315c092c8807f9e5d8840c2f0468bb8d98b4f060
angeolson <78054238+angeolson@users.noreply.github.com>
2022-04-02 13:01:35 -0400

    Changed preprocessing file

--- preprocessing.py
+++ preprocessing.py
@@ -4,16 +4,17 @@ import pandas as pd
 import dm6103 as dm
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 #%%
 # read in datafile and check variables 
 df = pd.read_csv("heart_2020_cleaned.csv")
-dm.dfChk(df)
+#dm.dfChk(df)
 
 #%%
 # change age variable to continuous 
 
-df["AgeCategory"].value_counts()
+#df["AgeCategory"].value_counts()
 
 #65-69          34151
 #60-64          33686
@@ -36,31 +37,31 @@ def cleanDfAge(row):
 
   thisage = thisage.strip()
   if thisage == "18-24": 
-      thisage = min(round(18 + 6*np.random.random()), 24)
+      thisage = np.random.uniform(low = 18, high = 24)
   if thisage == "25-29": 
-      thisage = min(round(25 + 4*np.random.random()), 29)
-  if thisage == "30-34": 
-      thisage = min(round(30 + 4*np.random.random()), 34)
+      thisage = np.random.uniform(low = 25, high = 29)
+  if thisage == "30-34":
+      thisage = np.random.uniform(low = 30, high = 34)
   if thisage == "35-39": 
-      thisage = min(round(35 + 4*np.random.random()), 39)
+      thisage = np.random.uniform(low = 35, high = 39)
   if thisage == "40-44": 
-      thisage = min(round(40 + 4*np.random.random()), 44)
+      thisage = np.random.uniform(low = 40, high = 44)
   if thisage == "45-49": 
-      thisage = min(round(45 + 4*np.random.random()), 49)
+      thisage = np.random.uniform(low = 45, high = 49)
   if thisage == "50-54": 
-      thisage = min(round(50 + 4*np.random.random()), 54)
+      thisage = np.random.uniform(low = 50, high = 54)
   if thisage == "55-59": 
-      thisage = min(round(55 + 4*np.random.random()), 59)
+      thisage = np.random.uniform(low = 55, high = 59)
   if thisage == "60-64": 
-      thisage = min(round(60 + 4*np.random.random()), 64)
+      thisage = np.random.uniform(low = 60, high = 64)
   if thisage == "65-69": 
-      thisage = min(round(65 + 4*np.random.random()), 69)
+      thisage = np.random.uniform(low = 65, high = 69)
   if thisage == "70-74": 
-      thisage = min(round(70 + 4*np.random.random()), 74)
+      thisage = np.random.uniform(low = 70, high = 74)
   if thisage == "75-79": 
-      thisage = min(round(75 + 4*np.random.random()), 79)
+      thisage = np.random.uniform(low = 75, high = 79)
   if thisage == "80 or older": 
-      thisage = min(round(80 + 3*np.random.chisquare(2)), 100)
+      thisage = min(80 + 3*np.random.chisquare(2), 99)
   
   return thisage 
 # end function cleanDfAge
@@ -69,6 +70,12 @@ print("\nReady to continue.")
 df["AgeCategory"] = df.apply(cleanDfAge,axis=1)
 print(df.dtypes)
 print("\nReady to continue.")
+
+# %%
+# checking age variable distribution
+sns.set(style="whitegrid")
+sns.distplot(x=df['AgeCategory'], kde = False)
+plt.show()
 
 # %%
 # export clean dataset 

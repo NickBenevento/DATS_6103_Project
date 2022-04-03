828af30a43714ced3c5789b6cc0d536362644ecd
NickBenevento <nbenevento99@gmail.com>
2022-04-03 14:30:21 -0400

    Update age cleaning to include end ranges
    - fixes issue of non-smooth age distribution after cleaning

--- preprocessing.py
+++ preprocessing.py
@@ -36,38 +36,25 @@ def cleanDfAge(row):
   thisage = row["AgeCategory"]
 
   thisage = thisage.strip()
-  if thisage == "18-24": 
-      thisage = np.random.uniform(low = 18, high = 24)
-  if thisage == "25-29": 
-      thisage = np.random.uniform(low = 25, high = 29)
-  if thisage == "30-34":
-      thisage = np.random.uniform(low = 30, high = 34)
-  if thisage == "35-39": 
-      thisage = np.random.uniform(low = 35, high = 39)
-  if thisage == "40-44": 
-      thisage = np.random.uniform(low = 40, high = 44)
-  if thisage == "45-49": 
-      thisage = np.random.uniform(low = 45, high = 49)
-  if thisage == "50-54": 
-      thisage = np.random.uniform(low = 50, high = 54)
-  if thisage == "55-59": 
-      thisage = np.random.uniform(low = 55, high = 59)
-  if thisage == "60-64": 
-      thisage = np.random.uniform(low = 60, high = 64)
-  if thisage == "65-69": 
-      thisage = np.random.uniform(low = 65, high = 69)
-  if thisage == "70-74": 
-      thisage = np.random.uniform(low = 70, high = 74)
-  if thisage == "75-79": 
-      thisage = np.random.uniform(low = 75, high = 79)
-  if thisage == "80 or older": 
-      thisage = min(80 + 3*np.random.chisquare(2), 99)
-  
-  return thisage 
+  if thisage == "18-24": return np.random.uniform(18, 24)
+  if thisage == "25-29": return np.random.uniform(24, 29)
+  if thisage == "30-34": return np.random.uniform(29, 34)
+  if thisage == "35-39": return np.random.uniform(34, 39)
+  if thisage == "40-44": return np.random.uniform(39, 44)
+  if thisage == "45-49": return np.random.uniform(44, 49)
+  if thisage == "50-54": return np.random.uniform(49, 54)
+  if thisage == "55-59": return np.random.uniform(54, 59)
+  if thisage == "60-64": return np.random.uniform(59, 64)
+  if thisage == "65-69": return np.random.uniform(64, 69)
+  if thisage == "70-74": return np.random.uniform(69, 74)
+  if thisage == "75-79": return np.random.uniform(74, 79)
+  if thisage == "80 or older": return min(79 + 3*np.random.chisquare(2), 99)
+  return np.nan
+
 # end function cleanDfAge
 print("\nReady to continue.")
 
-df["AgeCategory"] = df.apply(cleanDfAge,axis=1)
+df["AgeCategory"] = df.apply(cleanDfAge, axis=1)
 print(df.dtypes)
 print("\nReady to continue.")
 

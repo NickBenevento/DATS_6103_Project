7e778f79173cc6d0b29138981a6f68a7550c33a8
Yaxin(Janet) Zhuang <49347418+yqz5514@users.noreply.github.com>
2022-04-05 10:08:27 -0400

    Update demo_janet.py

---
+++ demo_janet.py
@@ -0,0 +1,13 @@
+#%%
+# import packages
+import pandas as pd
+import dm6103 as dm
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+#%%
+# read in datafile and check variables 
+df = pd.read_csv("heart_2020_new.csv")
+dm.dfChk(df)
+# %%

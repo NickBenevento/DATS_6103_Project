30de4addaee470b01201564f28593c503e33933f
angeolson <78054238+angeolson@users.noreply.github.com>
2022-04-06 10:56:53 -0400

    Add statistical tests
    Added t tests and chi sq tests for reach plot to show statistical significance of findings

--- demo_janet.py
+++ demo_janet.py
@@ -98,8 +98,14 @@ new_df.head()
 df.to_csv('heart_2020_balanced.csv')  
 print("\nReady to continue.")
 
+
+# stat test prep
+import scipy.stats as stats
+df_HD = new_df[ new_df['HeartDisease'] == 'Yes']
+df_NO = new_df[ new_df['HeartDisease'] == 'No']
+
 #%%
-#BMI and Heart Disease
+# BMI and Heart Disease
 plt.figure(figsize=(10,4))
 
 # Histogram
@@ -112,11 +118,20 @@ plt.xlabel('BMI indicator')
 # 28 and the more BMI this leads to increase HeartDisease, 
 # and that's make sense as BMI is indictator for obesity whic is one of direct cause of HeartDisease¶
 
+# t test
+print("t test p val: \n", stats.ttest_ind(df_HD['BMI'], df_NO['BMI'])[1])
+# stat sig at a = 0.01, confirm that there is a difference in means
+
 #%%
 # #Smoking and heart disease
 plt.figure(figsize=(10,4))
 sns.countplot(data=new_df, x='Smoking', hue='HeartDisease')
 #We notice that smoking increase your chance in getting Heart Diseases by about 50%
+
+# chi sq
+ct1 = pd.crosstab(new_df['Smoking'], new_df['HeartDisease'])
+print("chi sq p val: \n", stats.chi2_contingency(ct1)[1])
+# stat sig at a = 0.01, confirm that there is a difference in frequencies
 
 #%%
 #Alchol and Heart Disease
@@ -125,10 +140,19 @@ sns.countplot(data=new_df, x='AlcoholDrinking', hue='HeartDisease')
 #drinking alchol don't increase your chance by getting heart disease on the other side the people who don't drink and get heart disease 
 # is higher than the people who don't drink and get heart disease
 
+# chi sq
+ct2 = pd.crosstab(new_df['AlcoholDrinking'], new_df['HeartDisease'])
+print("chi sq p val: \n", stats.chi2_contingency(ct2)[1])
+# stat sig at a = 0.01, confirm that there is a difference in frequencies
 #%%
-#Emoitional Stroke and heartdisease
+#Emotional Stroke and heartdisease
 plt.figure(figsize=(10,4))
 sns.countplot(data=new_df, x='Stroke', hue='HeartDisease')
+
+# chi sq
+ct3 = pd.crosstab(new_df['Stroke'], new_df['HeartDisease'])
+print("chi sq p val: \n", stats.chi2_contingency(ct3)[1])
+# stat sig at a = 0.01, confirm that there is a difference in frequencies
 
 #%%
 #Difficulty in walking and heart disease
@@ -138,6 +162,10 @@ sns.countplot(data=new_df, x='DiffWalking', hue='HeartDisease')
 #It's completely related as the people who have heart disease will be more 
 # likely to have difficulty in walking or climibing stairs with a precentage = 300%¶
 
+# chi sq
+ct4 = pd.crosstab(new_df['DiffWalking'], new_df['HeartDisease'])
+print("chi sq p val: \n", stats.chi2_contingency(ct4)[1])
+# stat sig at a = 0.01, confirm that there is a difference in frequencies
 
 #%%
 #Gender and Heart Disease
@@ -145,21 +173,41 @@ plt.figure(figsize=(10,4))
 sns.countplot(data=new_df, x='Sex', hue='HeartDisease')
 #Males have more chance of getting heartdisease
 
+# chi sq
+ct5 = pd.crosstab(new_df['Sex'], new_df['HeartDisease'])
+print("chi sq p val: \n", stats.chi2_contingency(ct5)[1])
+# stat sig at a = 0.01, confirm that there is a difference in frequencies
+
 #%%
 #Age category and heart disease
 plt.figure(figsize=(16,4))
 sns.countplot(data=new_df, x='AgeCategory', hue='HeartDisease', order = new_df['AgeCategory'].sort_values().unique())
 #Increasing age will lead to higher chances to get heart diseases
 
+# chi sq
+ct6 = pd.crosstab(new_df['AgeCategory'], new_df['HeartDisease'])
+print("chi sq p val: \n", stats.chi2_contingency(ct6)[1])
+# stat sig at a = 0.01, confirm that there is a difference in frequencies
+
+#%%
 # AgeCont (continuous age variable) and heart disease 
 sns.boxplot(x='HeartDisease',y='AgeCont', data=new_df)
 # also looks like two different mean ages, with higher age associated with heart disease
+
+# t test
+print("t test p val: \n", stats.ttest_ind(df_HD['AgeCont'], df_NO['AgeCont'])[1])
+# stat sig at a = 0.01, confirm that there is a difference in means
 
 #%%
 #Human race and heart disease
 plt.figure(figsize=(16,6))
 sns.countplot(data=new_df, x='Race', hue='HeartDisease')
 #there is a remarkable drop in the Asian people with heart disease
+
+# chi sq
+ct7 = pd.crosstab(new_df['Race'], new_df['HeartDisease'])
+print("chi sq p val: \n", stats.chi2_contingency(ct7)[1])
+# stat sig at a = 0.01, confirm that there is a difference in frequencies
 
 #%%
 #Diabete and heart disease
@@ -169,6 +217,12 @@ sns.countplot(data=new_df, x='Diabetic', hue='HeartDisease')
 #Diabetic people have chance to be diagnoised by heart disease 
 # with increasing chance = 300%
 
+# chi sq
+ct8 = pd.crosstab(new_df['Diabetic'], new_df['HeartDisease'])
+print("chi sq p val: \n", stats.chi2_contingency(ct8)[1])
+# stat sig at a = 0.01, confirm that there is a difference in frequencies
+
+
 
 #%%
 #Physical activity and heart disease
@@ -177,17 +231,33 @@ sns.countplot(data=new_df, x='PhysicalActivity', hue='HeartDisease')
 
 #Logically, practicing sport will reduce the chance of getting heart disease, and not doing sport double your chance to get heart diseases
 
+# chi sq
+ct9 = pd.crosstab(new_df['PhysicalActivity'], new_df['HeartDisease'])
+print("chi sq p val: \n", stats.chi2_contingency(ct9)[1])
+# stat sig at a = 0.01, confirm that there is a difference in frequencies
+
 #%%
 #GenHealth and heart disease
 plt.figure(figsize=(16,4))
 sns.countplot(data=new_df, x='GenHealth', hue='HeartDisease')
 #the more better GenHealth the less chance to get heart diseases
 
+# chi sq
+ct10 = pd.crosstab(new_df['GenHealth'], new_df['HeartDisease'])
+print("chi sq p val: \n", stats.chi2_contingency(ct10)[1])
+# stat sig at a = 0.01, confirm that there is a difference in frequencies
+
+
 #%%
 #SleepTime and heart disease
 plt.figure(figsize=(16,6))
 sns.histplot(data=new_df, x='SleepTime', hue='HeartDisease', binwidth=1, binrange= (0,15))
-#We notice that the best sleeping routine would be to get 7.5 hourse per night to reduce the chance of getting heart disease
+#We notice that the best sleeping routine would be to get 7.5 hours per night to reduce the chance of getting heart disease
+
+# t test
+print("t test p val: \n", stats.ttest_ind(df_HD['SleepTime'], df_NO['SleepTime'])[1])
+# stat sig at a = 0.01, confirm that there is a difference in means
+
 
 #%%
 #Asthma and heart disease
@@ -195,12 +265,27 @@ plt.figure(figsize=(10,4))
 sns.countplot(data=new_df, x='Asthma', hue='HeartDisease')
 #Having Asthma will increase your chance to get heart disease
 
+# chi sq
+ct11 = pd.crosstab(new_df['Asthma'], new_df['HeartDisease'])
+print("chi sq p val: \n", stats.chi2_contingency(ct11)[1])
+# stat sig at a = 0.01, confirm that there is a difference in frequencies
+
 #%%
 #Kidney diesase and heart disease
 plt.figure(figsize=(10,4))
 sns.countplot(data=new_df, x='KidneyDisease', hue='HeartDisease')
 
+# chi sq
+ct12 = pd.crosstab(new_df['KidneyDisease'], new_df['HeartDisease'])
+print("chi sq p val: \n", stats.chi2_contingency(ct12)[1])
+# stat sig at a = 0.01, confirm that there is a difference in frequencies
 #%%
 #SkinCancer and heart disease
 plt.figure(figsize=(10,4))
 sns.countplot(data=new_df, x='SkinCancer', hue='HeartDisease')
+
+# chi sq
+ct13 = pd.crosstab(new_df['SkinCancer'], new_df['HeartDisease'])
+print("chi sq p val: \n", stats.chi2_contingency(ct13)[1])
+# stat sig at a = 0.01, confirm that there is a difference in frequencies
+# %%

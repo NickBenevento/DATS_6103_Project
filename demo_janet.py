3a638eaac20a262380e89d9c3f3eec410189a6af
angeolson <78054238+angeolson@users.noreply.github.com>
2022-04-06 10:26:08 -0400

    Edit demo_janet and save data as .csv file
    Added a continuous variable for age as well as categorical , and added a boxplot to show difference in age. Exported dataset to .csv to use in the future.

--- demo_janet.py
+++ demo_janet.py
@@ -10,6 +10,7 @@ import seaborn as sns
 # read in datafile and check variables 
 df = pd.read_csv("heart_2020_cleaned_default.csv")
 dm.dfChk(df)
+
 # %%
 df.describe()
 # %%
@@ -59,6 +60,45 @@ new_df.reset_index(inplace = True,drop = True)
 new_df
 
 #%%
+# add new variable for continuous age (like in 'heart_2020_new.csv)
+# define function
+
+def cleanDfAge(row):
+  thisage = row["AgeCategory"]
+
+  thisage = thisage.strip()
+  if thisage == "18-24": return np.random.uniform(18, 25)
+  if thisage == "25-29": return np.random.uniform(25, 30)
+  if thisage == "30-34": return np.random.uniform(30, 35)
+  if thisage == "35-39": return np.random.uniform(35, 40)
+  if thisage == "40-44": return np.random.uniform(40, 45)
+  if thisage == "45-49": return np.random.uniform(45, 50)
+  if thisage == "50-54": return np.random.uniform(50, 55)
+  if thisage == "55-59": return np.random.uniform(55, 60)
+  if thisage == "60-64": return np.random.uniform(60, 65)
+  if thisage == "65-69": return np.random.uniform(65, 70)
+  if thisage == "70-74": return np.random.uniform(70, 75)
+  if thisage == "75-79": return np.random.uniform(75, 80)
+  if thisage == "80 or older": return min(79 + 3*np.random.chisquare(2), 99)
+  return np.nan
+
+# end function cleanDfAge
+print("\nReady to continue.")
+
+# apply function and return new column of data
+new_df["AgeCont"] = new_df.apply(cleanDfAge, axis=1)
+print(new_df.dtypes)
+print("\nReady to continue.")
+
+# check that this works
+new_df.head()
+
+# %%
+# export dataset for us to use later
+df.to_csv('heart_2020_balanced.csv')  
+print("\nReady to continue.")
+
+#%%
 #BMI and Heart Disease
 plt.figure(figsize=(10,4))
 
@@ -71,17 +111,20 @@ plt.xlabel('BMI indicator')
 #We notice that the most healthy people who have BMI within 20 till about 
 # 28 and the more BMI this leads to increase HeartDisease, 
 # and that's make sense as BMI is indictator for obesity whic is one of direct cause of HeartDisease¶
+
 #%%
 # #Smoking and heart disease
 plt.figure(figsize=(10,4))
 sns.countplot(data=new_df, x='Smoking', hue='HeartDisease')
 #We notice that smoking increase your chance in getting Heart Diseases by about 50%
+
 #%%
 #Alchol and Heart Disease
 plt.figure(figsize=(10,4))
 sns.countplot(data=new_df, x='AlcoholDrinking', hue='HeartDisease')
 #drinking alchol don't increase your chance by getting heart disease on the other side the people who don't drink and get heart disease 
 # is higher than the people who don't drink and get heart disease
+
 #%%
 #Emoitional Stroke and heartdisease
 plt.figure(figsize=(10,4))
@@ -107,6 +150,10 @@ sns.countplot(data=new_df, x='Sex', hue='HeartDisease')
 plt.figure(figsize=(16,4))
 sns.countplot(data=new_df, x='AgeCategory', hue='HeartDisease', order = new_df['AgeCategory'].sort_values().unique())
 #Increasing age will lead to higher chances to get heart diseases
+
+# AgeCont (continuous age variable) and heart disease 
+sns.boxplot(x='HeartDisease',y='AgeCont', data=new_df)
+# also looks like two different mean ages, with higher age associated with heart disease
 
 #%%
 #Human race and heart disease

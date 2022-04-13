effddd3e1434fd749abc520526c7239448a80934
angeolson <78054238+angeolson@users.noreply.github.com>
2022-04-13 11:15:41 -0400

    Update regression.py

--- regression.py
+++ regression.py
@@ -77,40 +77,6 @@ for cut_off in cut_offs:
 df2 = pd.read_csv("heart_2020_balanced.csv")
 dm.dfChk(df2)
 
-#%%
-# add new variable for continuous age (like in 'heart_2020_new.csv)
-# define function
-
-def cleanDfAge(row):
-  thisage = row["AgeCategory"]
-
-  thisage = thisage.strip()
-  if thisage == "18-24": return np.random.uniform(18, 25)
-  if thisage == "25-29": return np.random.uniform(25, 30)
-  if thisage == "30-34": return np.random.uniform(30, 35)
-  if thisage == "35-39": return np.random.uniform(35, 40)
-  if thisage == "40-44": return np.random.uniform(40, 45)
-  if thisage == "45-49": return np.random.uniform(45, 50)
-  if thisage == "50-54": return np.random.uniform(50, 55)
-  if thisage == "55-59": return np.random.uniform(55, 60)
-  if thisage == "60-64": return np.random.uniform(60, 65)
-  if thisage == "65-69": return np.random.uniform(65, 70)
-  if thisage == "70-74": return np.random.uniform(70, 75)
-  if thisage == "75-79": return np.random.uniform(75, 80)
-  if thisage == "80 or older": return min(79 + 3*np.random.chisquare(2), 99)
-  return np.nan
-
-# end function cleanDfAge
-print("\nReady to continue.")
-
-# apply function and return new column of data
-df2["AgeCont"] = df2.apply(cleanDfAge, axis=1)
-print(df2.dtypes)
-print("\nReady to continue.")
-
-# check that this works
-df2.head()
-
 # %%
 # create binary variable for Heart Disease 
 df2['HeartDiseaseBin'] = 0

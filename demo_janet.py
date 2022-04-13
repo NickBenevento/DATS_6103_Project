c878fd5feeca29c9a03d3bdf5b0a5e65b98663b4
Yaxin(Janet) Zhuang <49347418+yqz5514@users.noreply.github.com>
2022-04-13 15:14:12 -0400

    Update demo_janet.py

--- demo_janet.py
+++ demo_janet.py
@@ -46,19 +46,65 @@ new_df.head()
 
 print('Distribution of the Classes in the subsample dataset')
 print(new_df['HeartDisease'].value_counts()/len(new_df))
-
-
+#%%
 #%%
 sns.countplot('HeartDisease', data=new_df)
 plt.title('Equally Distributed Classes', fontsize=14)
 plt.show()
-
 #%%
 #Reset indices
 new_df.reset_index(inplace = True,drop = True)
 new_df
 
 #%%
+# pie chart
+# Create the function to visualize to pie chart
+def Draw_pie_chart(name_feature):
+
+    fig,axes = plt.subplots(1,2,figsize=(15,8))
+    labels = new_df[name_feature].unique()
+    textprops = {"fontsize":15}
+
+    axes[0].pie(new_df[new_df.HeartDisease=="No"][name_feature].value_counts(), labels=labels,autopct='%1.1f%%',textprops =textprops)
+    axes[0].set_title('No Heart Disease',fontsize=15)
+    axes[1].pie(new_df[new_df.HeartDisease=="Yes"][name_feature].value_counts(), labels=labels,autopct='%1.1f%%',textprops =textprops)
+    axes[1].set_title('Yes Heart Disease',fontsize=15)
+
+    plt.legend(title = name_feature, fontsize=15, title_fontsize=15)
+    plt.show()
+    
+#%%
+# pie chart
+Draw_pie_chart("Smoking")
+
+#%%
+features = new_df.columns
+# Take out all features is binary data (Yes, No) and (Male, Female)
+binary_feature = []
+
+for feature in new_df.columns:
+    if np.isin(new_df[feature].unique(),["Yes","No"]).all() or np.isin(new_df[feature].unique(),["Male","Female"]).all():
+        binary_feature.append(feature)
+        
+# Take out all features is continuous data
+continuos_feature = ["BMI"]
+
+# Take out all features is discrete data
+discrete_feature = features[~features.isin(binary_feature+continuos_feature)]
+
+
+#%%
+for feature in binary_feature[2:]:
+    Draw_pie_chart(feature)
+
+#%%
+sns.countplot('HeartDisease', data=new_df)
+plt.title('Equally Distributed Classes', fontsize=14)
+plt.show()
+
+
+#%%
+# countinues variable
 #BMI and Heart Disease
 plt.figure(figsize=(10,4))
 
@@ -71,39 +117,10 @@ plt.xlabel('BMI indicator')
 #We notice that the most healthy people who have BMI within 20 till about 
 # 28 and the more BMI this leads to increase HeartDisease, 
 # and that's make sense as BMI is indictator for obesity whic is one of direct cause of HeartDisease¶
-#%%
-# #Smoking and heart disease
-plt.figure(figsize=(10,4))
-sns.countplot(data=new_df, x='Smoking', hue='HeartDisease')
-#We notice that smoking increase your chance in getting Heart Diseases by about 50%
-# claculation
-#%%
-#Alchol and Heart Disease
-plt.figure(figsize=(10,4))
-sns.countplot(data=new_df, x='AlcoholDrinking', hue='HeartDisease')
-#drinking alchol don't increase your chance by getting heart disease on the other side the people who don't drink and get heart disease 
-# is higher than the people who don't drink and get heart disease
-#%%
-#Emoitional Stroke and heartdisease
-plt.figure(figsize=(10,4))
-sns.countplot(data=new_df, x='Stroke', hue='HeartDisease')
-
-#%%
-#Difficulty in walking and heart disease
-plt.figure(figsize=(10,4))
-sns.countplot(data=new_df, x='DiffWalking', hue='HeartDisease')
-
-#It's completely related as the people who have heart disease will be more 
-# likely to have difficulty in walking or climibing stairs with a precentage = 300%¶
 
 
 #%%
-#Gender and Heart Disease
-plt.figure(figsize=(10,4))
-sns.countplot(data=new_df, x='Sex', hue='HeartDisease')
-#Males have more chance of getting heartdisease
-
-#%%
+# disceret variable
 #Age category and heart disease
 plt.figure(figsize=(16,4))
 sns.countplot(data=new_df, x='AgeCategory', hue='HeartDisease', order = new_df['AgeCategory'].sort_values().unique())
@@ -125,13 +142,6 @@ sns.countplot(data=new_df, x='Diabetic', hue='HeartDisease')
 
 
 #%%
-#Physical activity and heart disease
-plt.figure(figsize=(10,4))
-sns.countplot(data=new_df, x='PhysicalActivity', hue='HeartDisease')
-
-#Logically, practicing sport will reduce the chance of getting heart disease, and not doing sport double your chance to get heart diseases
-
-#%%
 #GenHealth and heart disease
 plt.figure(figsize=(16,4))
 sns.countplot(data=new_df, x='GenHealth', hue='HeartDisease')
@@ -142,21 +152,3 @@ sns.countplot(data=new_df, x='GenHealth', hue='HeartDisease')
 plt.figure(figsize=(16,6))
 sns.histplot(data=new_df, x='SleepTime', hue='HeartDisease', binwidth=1, binrange= (0,15))
 #We notice that the best sleeping routine would be to get 7.5 hourse per night to reduce the chance of getting heart disease
-
-#%%
-#Asthma and heart disease
-plt.figure(figsize=(10,4))
-sns.countplot(data=new_df, x='Asthma', hue='HeartDisease')
-#Having Asthma will increase your chance to get heart disease
-
-#%%
-#Kidney diesase and heart disease
-plt.figure(figsize=(10,4))
-sns.countplot(data=new_df, x='KidneyDisease', hue='HeartDisease')
-
-#%%
-#SkinCancer and heart disease
-plt.figure(figsize=(10,4))
-sns.countplot(data=new_df, x='SkinCancer', hue='HeartDisease')
-
-#%%

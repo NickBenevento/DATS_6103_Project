e9cae8b89302cd0b7acafa2ff28aae3d3e3a5f13
angeolson <78054238+angeolson@users.noreply.github.com>
2022-04-15 18:04:53 -0400

    Update sklearn_logit.py

--- sklearn_logit.py
+++ sklearn_logit.py
@@ -16,14 +16,34 @@ df.loc[ df['HeartDisease'] == "Yes", 'HeartDiseaseBin' ] = 1
 # %%
 # prep data for modelling
 
-dfX = df[[ 'Smoking', 'AlcoholDrinking', 'Stroke', 'PhysicalHealth', 'MentalHealth', 'DiffWalking', 'Sex', 'Race', 'Diabetic', 'PhysicalActivity', 'GenHealth', 'SleepTime', 'Asthma', 'KidneyDisease', 'SkinCancer', 'AgeCont']]
+dfX = df[[ 'Smoking', 'AlcoholDrinking', 'Stroke', 'PhysicalHealth', 'MentalHealth', 'DiffWalking', 'Sex', 'Race', 'Diabetic', 'PhysicalActivity', 'GenHealth', 'SleepTime', 'Asthma', 'KidneyDisease', 'SkinCancer', 'AgeCont', 'BMI']]
 
 dfY = df[['HeartDiseaseBin']]
 
+# change X variables to either numeric or factor
+
+for var in ('Smoking', 'AlcoholDrinking', 'Stroke',  'DiffWalking', 'Sex', 'Race', 'Diabetic', 'PhysicalActivity', 'GenHealth',  'Asthma', 'KidneyDisease', 'SkinCancer'):
+    dfX[var] = dfX[var].astype('category').cat.codes
+
 # split into test and train 
-X_train, X_test, y_train, y_test = train_test_split(DfX, DfY, test_size = 0.25, random_state=333)
+X_train, X_test, y_train, y_test = train_test_split(dfX, dfY, test_size = 0.25, random_state=333)
 
 # create empty dataframe for predictions vs. actuals
 model_predictions = pd.DataFrame()
+model_predictions['Actuals'] = y_test['HeartDiseaseBin']
+
+# %%
+# model
+
+HDlogit = LogisticRegression()  # instantiate
+HDlogit.fit(X_train, y_train)
+print('Logit model accuracy (with the test set):', HDlogit.score(X_test, y_test))
+print('Logit model accuracy (with the train set):', HDlogit.score(X_train, y_train))
 
 
+# %%
+# Store predictions
+model_predictions['Model1'] = HDlogit.predict_proba(X_test)[:,1]
+model_predictions.head()
+
+# %%

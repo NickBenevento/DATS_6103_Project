2af7adaee9b69b685c0da14757aab3c9de6d9315
NickBenevento <nbenevento99@gmail.com>
2022-04-16 18:13:03 -0400

    Add precision, recall, and f1

--- svm.py
+++ svm.py
@@ -3,7 +3,7 @@ import pandas as pd
 import dm6103 as dm
 from sklearn.model_selection import train_test_split
 from sklearn.svm import SVC
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score
 
 
 df = pd.read_csv('heart_2020_balanced.csv')
@@ -24,13 +24,20 @@ for var in ('Smoking', 'AlcoholDrinking', 'Stroke',  'DiffWalking', 'Sex', 'Race
 
 X_train, X_test, y_train, y_test = train_test_split(x_df, y_df, test_size = 0.25, random_state=333)
 # %%
-# use a linear kernel b/c we are predicting a binary variable (2 dimensions)
+# use a linear kernel b/c we are predicting a binary variable (2 dimensions for hyperplane)
 svm = SVC(kernel='linear')
 svm.fit(X_train, y_train.values.ravel())
 
 y_pred = svm.predict(X_test)
 
 # %%
-print('Accuracy: ', accuracy_score(y_test, y_pred))
-# 74.74% accuracy; decent
+recall = precision_score(y_test, y_pred)
+precision = recall_score(y_test, y_pred)
+accuracy = accuracy_score(y_test, y_pred)
+F1 = f1_score(y_test, y_pred)
+print('Recall: ', round(recall, 4))
+print('Precision: ', round(precision, 4))
+print('Accuracy: ', round(accuracy, 4))
+print('F1: ', round(F1, 4))
+# F1: 75.34%, accuracy: 74.74%: decent
 # %%

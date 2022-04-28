f8648e727ffd1bd9cab5d7978f5aa6edecfb5b14
Yaxin(Janet) Zhuang <49347418+yqz5514@users.noreply.github.com>
2022-04-28 12:12:13 -0400

    Update EDA1.py

--- EDA1.py
+++ EDA1.py
@@ -15,16 +15,124 @@ df.describe()
 # %%
 for col in df:
     print(col, df[col].unique())
+    
+#%%
+#### general plot
+sns.set(style="whitegrid")
+
+# stat test prep
+import scipy.stats as stats
+df_HD = df[ df['HeartDisease'] == 'Yes']
+df_NO = df[ df['HeartDisease'] == 'No']
+
+
+#%%
+# Boxplots
+
+for var in list:
+    sns.boxplot(x='HeartDisease',y=var, data=df).set(title = var)
+    plt.show()
+
+#%%
+# t tests for cont. variables
+
+for var in list:
+    print(var, "t test p val: \n", stats.ttest_ind(df_HD[var], df_NO[var])[1])
+
+# evidence for statistically significantly different means for all variables.
+#%%
+# Barcharts 
+
+## HeartDisease and Race
+
+# create crosstab
+ct=pd.crosstab(columns=df['Race'],index=df['HeartDisease'])
+#print(ct)
+
+# define vars to make proportions
+dfwhite = df[ df['Race'] == "White" ]
+white = dfwhite.count()[0]
+
+dfaian = df[ df['Race'] == "American Indian/Alaskan Native" ]
+aian = dfaian.count()[0]
+
+dfasian = df[ df['Race'] == "Asian" ]
+asian = dfasian.count()[0]
+
+dfblack = df[ df['Race'] == "Black" ]
+black = dfblack.count()[0]
+
+dfhis = df[ df['Race'] == "Hispanic" ]
+his = dfhis.count()[0]
+
+dfoth = df[ df['Race'] == "Other" ]
+oth = dfoth.count()[0]
+
+# now stack and reset
+
+stacked = ct.stack().reset_index().rename(columns={0:'value'})
+
+# create proportion column
+stacked['valueProp'] = 0
+stacked.loc[ stacked['Race'] == "White", 'valueProp'] = stacked['value']/white
+stacked.loc[ stacked['Race'] == "American Indian/Alaskan Native", 'valueProp'] = stacked['value']/aian
+stacked.loc[ stacked['Race'] == "Asian", 'valueProp'] = stacked['value']/asian
+stacked.loc[ stacked['Race'] == "Black", 'valueProp'] = stacked['value']/black
+stacked.loc[ stacked['Race'] == "Hispanic", 'valueProp'] = stacked['value']/his
+stacked.loc[ stacked['Race'] == "Other", 'valueProp'] = stacked['value']/oth
+
+# plot grouped bar chart
+p = sns.barplot(x=stacked.Race, y=stacked.valueProp, hue=stacked.HeartDisease)
+sns.move_legend(p, bbox_to_anchor=(1, 1.02), loc='upper left')
+p.set(ylabel = "Share of Total")
+plt.show()
+
+# other bar charts
+
+list1 = ('Sex', 'Smoking', 'AlcoholDrinking', 'Stroke', 'DiffWalking', 'PhysicalActivity', 'Asthma', 'KidneyDisease', 'SkinCancer')
+list2 = ('GenHealth', 'Diabetic')
+
+# binary
+for var in list1:
+    # create crosstab
+    ct=pd.crosstab(columns=df[var],index=df['HeartDisease'])
+    # now stack and reset
+    stacked = ct.stack().reset_index().rename(columns={0:'value'})
+    # plot grouped bar chart
+    p = sns.barplot(x=stacked['HeartDisease'], y=stacked['value'], hue=stacked[var])
+    sns.move_legend(p, bbox_to_anchor=(1, 1.02), loc='upper left')
+    p.set(ylabel = "Count")
+    plt.show()
+
+# multi-level
+for var in list2:
+    # create crosstab
+    ct=pd.crosstab(columns=df['HeartDisease'],index=df[var])
+    # now stack and reset
+    stacked = ct.stack().reset_index().rename(columns={0:'value'})
+    # plot grouped bar chart
+    p = sns.barplot(x=stacked[var], y=stacked['value'], hue=stacked['HeartDisease'])
+    sns.move_legend(p, bbox_to_anchor=(1, 1.02), loc='upper left')
+    p.set(ylabel = "Count")
+    plt.show()
+
+
+# %%
+# Chi-squared tests for categorical variables 
+list3 = list1 + list2
+
+for var in list3:
+    ct = pd.crosstab(df[var], df['HeartDisease'])
+    print(var, "chi sq p val: \n", stats.chi2_contingency(ct)[1])
+
+# evidence for statistically significantly different frequencies for all variables.
+# %%
+
 # %%
 #Check labeled values distrbuition
 sns.countplot(df['HeartDisease'])
 plt.show()
 # unbalance data
-# %%
-# the three key factors we are going to focus; smoking, high blood pressure, and high cholesterol
-#Smoking and heart disease
-plt.figure(figsize=(10,4))
-sns.countplot(data=df, x='Smoking', hue='HeartDisease')
 
 # %%
 # balance the data
@@ -195,7 +303,38 @@ df3 = new_df[new_df['PhysicalActivity'] == 'No']
 df3.groupby('HeartDisease').size().plot(kind='pie', autopct='%1.0f%%', textprops={'fontsize': 20},colors=['tomato', 'gold'])
 
 
+#%%
+############################
+# plot use not scaled dataset
+# binary 
+def Draw_pie_chart(name_feature):
 
+    fig,axes = plt.subplots(1,2,figsize=(15,8))
+    labels = df[name_feature].unique()
+    textprops = {"fontsize":15}
+
+    axes[0].pie(df[df.HeartDisease=="No"][name_feature].value_counts(), labels=labels,autopct='%1.1f%%',textprops =textprops)
+    axes[0].set_title('No Heart Disease',fontsize=15)
+    axes[1].pie(df[df.HeartDisease=="Yes"][name_feature].value_counts(), labels=labels,autopct='%1.1f%%',textprops =textprops)
+    axes[1].set_title('Yes Heart Disease',fontsize=15)
+
+    plt.legend(title = name_feature, fontsize=15, title_fontsize=15)
+    plt.show()
+    
+feature = df.columns
+# Take out all features is binary data (Yes, No) and (Male, Female)
+binary_features = []
+
+for f in df.columns:
+    if np.isin(df[f].unique(),["Yes","No"]).all() or np.isin(df[f].unique(),["Male","Female"]).all():
+        binary_features.append(f)
+        
+#%%
+# draw binary pie chart in un scaled dataset. 
+for f in binary_features[2:]:
+    Draw_pie_chart(f)
+
+# there are some variables become negative correlated with target variables after the data was scaled. 
 
 ##################################################################################
 #%%

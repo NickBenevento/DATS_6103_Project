a07293789de15acc6ec401c137a868175f258df4
NickBenevento <nbenevento99@gmail.com>
2022-04-28 21:34:21 -0400

    Add svm all observations

--- svm.py
+++ svm.py
@@ -1,6 +1,7 @@
 # %%
 import pandas as pd
 import dm6103 as dm
+import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score
@@ -15,6 +16,7 @@ df['HeartDiseaseBin'] = 0
 df.loc[ df['HeartDisease'] == "Yes", 'HeartDiseaseBin' ] = 1
 
 # %%
+features = [ 'Smoking', 'AlcoholDrinking', 'Stroke', 'PhysicalHealth', 'MentalHealth', 'DiffWalking', 'Sex', 'Race', 'Diabetic', 'PhysicalActivity', 'GenHealth', 'SleepTime', 'Asthma', 'KidneyDisease', 'SkinCancer', 'AgeCont', 'BMI']
 x_df = df[[ 'Smoking', 'AlcoholDrinking', 'Stroke', 'PhysicalHealth', 'MentalHealth', 'DiffWalking', 'Sex', 'Race', 'Diabetic', 'PhysicalActivity', 'GenHealth', 'SleepTime', 'Asthma', 'KidneyDisease', 'SkinCancer', 'AgeCont', 'BMI']]
 
 y_df = df[['HeartDiseaseBin']]
@@ -31,15 +33,55 @@ svm.fit(X_train, y_train.values.ravel())
 y_pred = svm.predict(X_test)
 
 # %%
+# all observations, no split
+all_observations = pd.DataFrame()
+all_observations['predicted'] = svm.predict(x_df)
+all_observations.head()
+
+# %%
+# Compute class predictions
+all_observations['heart_disease'] = np.where(all_observations['predicted'] == 1, 1, 0)
+#
+# Make a cross table
+confusion: pd.DataFrame = pd.crosstab(y_df.HeartDiseaseBin, all_observations['heart_disease'],
+                                      rownames=['Actual'], colnames=['Predicted'],
+                                      margins=True)
+# print(confusion)
+
+true_neg = confusion[0][0]
+false_pos = confusion[1][0]
+false_neg = confusion[0][1]
+true_pos = confusion[1][1]
+
+total = confusion['All']['All']
+accuracy = (confusion.iloc[1][1] + confusion.iloc[0][0]) / total
+precision = true_pos / (true_pos + false_pos)
+recall = true_pos / (true_pos + false_neg)
+f1 = 2 * (precision*recall) / (precision + recall)
+print('f1: ', round(f1, 3))
+print('accuracy: ', round(accuracy, 3))
+print('precision: ', round(precision, 3))
+print('recall: ', round(recall, 3))
+print()
+# %%
+from sklearn import metrics
+# Train/Test split
+
+y_true, y_pred = y_test, svm.predict(X_test)
+
 recall = precision_score(y_test, y_pred)
 precision = recall_score(y_test, y_pred)
 accuracy = accuracy_score(y_test, y_pred)
 F1 = f1_score(y_test, y_pred)
+
 print('Recall: ', round(recall, 4))
 print('Precision: ', round(precision, 4))
 print('Accuracy: ', round(accuracy, 4))
 print('F1: ', round(F1, 4))
+print(metrics.r2_score(y_true, y_pred))
+print(metrics.mean_squared_error(y_true, y_pred))
 # F1: 75.34%, accuracy: 74.74%: decent
+
 # %%
 # Use some test data:
 data = [[0, 0, 0, 0, 0, 0, 1, 5, 0, 1, 0, 8, 0, 0, 0, 40, 20],
